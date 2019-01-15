4f83b52eb68d65aea676475af4cd85d874e60bd8
mariaoca <mariaolidcamarena@gmail.com>
2019-01-15 19:12:20 +0100

    publish in github

---
+++ docs/precache-manifest.4086fe00a3dd3e0b6b10869c677e86ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "18533d5000bbf3d6af11",
+    "url": "./static/js/main.18533d50.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "./static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "18533d5000bbf3d6af11",
+    "url": "./static/css/main.d86079b8.chunk.css"
+  },
+  {
+    "revision": "42bb7daf7bd49e7b3097a3a42ea296bd",
+    "url": "./index.html"
+  }
+];
